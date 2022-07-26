611cb6c1b07b4a97e6bda5a3a42d8c63a943d963
James Dinh <jdinh@collabralink.com>
2022-07-26 12:23:13 -0700

    working through icon solutions

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,21 +1,44 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
 import { Icon } from "./Icon";
+import { iconChoices } from "./IconChoices";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "USWDS/Base/Icon",
   component: Icon,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {},
+  argTypes: {
+    iconSize: {
+      defaultValue: "3",
+      description:
+        "String used to specify the size of the icon, a number between 3-9",
+    },
+    icon: {
+      description: "Determines which icon that needs to be rendered",
+      options: iconChoices,
+      control: { type: "select" },
+    },
+    ariaHidden: {
+      description:
+        "If the icon is redundant and used solely as visual progressive enhancement, leave prop as default true",
+      defaultValue: true,
+    },
+    role: {
+      defaultValue: "img",
+      description:
+        "If the icon is redundant and used solely as visual progressive enhancement, leave prop as default img role.",
+    },
+  },
 } as ComponentMeta<typeof Icon>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Icon> = ({}) => <Icon />;
+const Template: ComponentStory<typeof Icon> = ({ icon, ...rest }) => (
+  <Icon icon={icon} {...rest} />
+);
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  text: "Test Icon",
+  icon: "accessibility_new",
 };
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,15 +2,26 @@ import React from "react";
 
 type IntrinsicElements = JSX.IntrinsicElements["svg"];
 
-interface Props extends IntrinsicElements {}
+interface Props extends IntrinsicElements {
+  icon?: string;
+  iconSize?: IconSize;
+  ariaHidden?: boolean;
+  role?: string;
+}
 
-type IconVariation = "";
+type IconSize = "3" | "4" | "5" | "6" | "7" | "8" | "9";
 
-export const Icon: React.FC<Props> = ({ ...rest }) => {
+export const Icon: React.FC<Props> = ({
+  icon,
+  iconSize = "3",
+  role = "img",
+  ariaHidden = true,
+  ...rest
+}) => {
   return (
     <>
-      <svg className="usa-icon" aria-hidden="true" focusable="false" role="img">
-        <use href="/src/assets/img/sprite.svg#accessibility_new"></use>
+      <svg {...rest} className={`usa-icon--size-${iconSize}`} focusable="false">
+        <use href={`sprite.svg#${icon}`}></use>
       </svg>
     </>
   );
