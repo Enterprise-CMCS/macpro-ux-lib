c48201fb6819189081c57115f938b3a8a0005454
jdinh8124 <jdinh@collabralink.com>
2022-08-01 23:52:08 +0000

    Deploying to gha-pages from  @ 470d1f48d9a6c12d273bc8353a3bf0d8334d6d91 🚀

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -10,6 +10,7 @@ interface Props extends IntrinsicElements {
   ariaHidden?: boolean;
   role?: string;
   ariaLabel?: string;
+  color?: string;
 }
 
 type IconSize = 3 | 4 | 5 | 6 | 7 | 8 | 9;
@@ -21,6 +22,7 @@ type IconSize = 3 | 4 | 5 | 6 | 7 | 8 | 9;
  * @param {boolean} [ariaHidden]          Determines whether or not the element is hidden from a screen reader.
  * @param {string}  [role]                Describes the role of an element in programs that can make use of it
  * @param {string}  [ariaLabel]           Error message text displayed when inputError === true.
+ * @param {color}   [string]              Color of Icon that is rendered, defaults to black.
  */
 
 export const Icon: React.FC<Props> = ({
@@ -29,6 +31,7 @@ export const Icon: React.FC<Props> = ({
   role = "img",
   ariaHidden = true,
   ariaLabel,
+  color = "black",
   ...rest
 }) => {
   const iconName = name.replace(/_/g, " ");
@@ -39,6 +42,8 @@ export const Icon: React.FC<Props> = ({
       className={`usa-icon--size-${iconSize}`}
       aria-hidden={ariaHidden}
       role={role}
+      fill="currentColor"
+      color={color}
     >
       <use href={`sprite.svg#${name}`}></use>
     </svg>
