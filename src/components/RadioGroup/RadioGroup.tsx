72902f9c916106607ac63ee76d4d1178d139fb7c
Gavin St. Ours <gst-ours@fearless.tech>
2022-08-09 09:11:41 -0400

    Improve comments

--- src/components/RadioGroup/RadioGroup.tsx
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -7,12 +7,13 @@ interface Props {
 }
 
 export const RadioGroup: React.FC<Props> = ({ groupName, radioProps }) => {
+  // Track which Radio is currently selected in order to show/hide child elements:
   const [selected, setSelected] = React.useState("");
   const handleChange = (e: React.ChangeEvent) => {
     setSelected(e.target.id);
   };
 
-  // This function creates Radio components based on an array of props objects
+  // Create Radio components for the RadioGroup based `radioProps`:
   const radioButtons = (radioProps: RadioProps[]): ReactElement[] => {
     let radios: ReactElement[] = [];
 
