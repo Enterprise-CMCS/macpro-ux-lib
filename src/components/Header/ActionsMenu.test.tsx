847eb3bf096fa59ae75a74c961b543021228a253
JP Hechter <jhechter@fearless.tech>
2022-08-09 13:47:56 -0400

    add ActionsMenu tests

--- src/components/Header/ActionsMenu.test.tsx
+++ src/components/Header/ActionsMenu.test.tsx
@@ -1,28 +1,55 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, screen, render } from "../../test-setup";
 
 import { ActionsMenu } from "./ActionsMenu";
+
+const testOnClick = jest.fn();
 
 const links = [
   {
     text: "Manage Profile",
     iconName: "person",
-    onClick: () => console.log("Manage Profile"),
+    href: "",
+    onClick: testOnClick,
   },
   {
     text: "Request Role Change",
     iconName: "people",
-    onClick: () => console.log("Request Role Change"),
+    href: "",
+    onClick: testOnClick,
   },
   {
     text: "Log Out",
     iconName: "logout",
-    onClick: () => console.log("Log Out"),
+    href: "",
+    onClick: testOnClick,
   },
 ];
 
 describe("Test the Header component", () => {
   it("should render", () => {
     render(<ActionsMenu name="My Account" links={links} />);
+
+    // menu should contain the correct items
+    ["Manage Profile", "Request Role Change", "Log Out"].forEach(
+      (text, idx) => {
+        const item = screen.getByText(text);
+        expect(item).toHaveAttribute("href");
+        fireEvent.click(item);
+        expect(testOnClick).toHaveBeenCalledTimes(idx + 1);
+      }
+    );
+  });
+
+  it("menu should open on click", () => {
+    const { container } = render(
+      <ActionsMenu name="My Account" links={links} />
+    );
+    // should render menu hidden
+    expect(container).toMatchSnapshot();
+
+    // expand menu
+    fireEvent.click(screen.getByText("My Account"));
+    expect(container).toMatchSnapshot();
   });
 });
