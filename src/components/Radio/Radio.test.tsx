1581f5033a78e6f77d1e36187a42a9af86173ac8
Gavin St. Ours <gst-ours@fearless.tech>
2022-08-10 10:18:58 -0400

    Cleanup and add snapshot tests

--- src/components/Radio/Radio.test.tsx
+++ src/components/Radio/Radio.test.tsx
@@ -49,58 +49,114 @@ describe("Radio", () => {
       expect(r.getAttribute("name")).toBe("default-group");
     });
   });
-});
 
-describe("Tile", () => {
-  beforeEach(() => {
-    render(
-      <form data-testid="test-form">
+  describe("Tile", () => {
+    beforeEach(() => {
+      render(
+        <form data-testid="test-form">
+          <Radio
+            id="radio-tile"
+            isTile={true}
+            label="Tile Radio"
+            name="tile-group"
+            tileDescription="This is the tile version of the Radio component."
+          />
+        </form>
+      );
+    });
+
+    it("renders as a tile", () => {
+      const r = screen.getByRole("radio");
+      expect(r).toHaveClass("usa-radio__input--tile");
+    });
+
+    it("is unchecked by default", () => {
+      const r = screen.getByRole("radio");
+      expect(r).not.toBeChecked();
+    });
+
+    it("becomes checked on click", () => {
+      const r = screen.getByRole("radio");
+      fireEvent.click(r);
+      expect(r).toBeChecked();
+    });
+
+    it("renders and has the expected label", () => {
+      const r = screen.getByRole("radio");
+      expect(r).toBeInTheDocument();
+    });
+
+    it("has the expected ID", () => {
+      const r = screen.getByRole("radio");
+      expect(r.getAttribute("id")).toBe("radio-tile");
+    });
+
+    it("has the expected name attribute", () => {
+      const r = screen.getByRole("radio");
+      expect(r.getAttribute("name")).toBe("tile-group");
+    });
+
+    it("has the expected description text", () => {
+      const testForm = screen.getByTestId("test-form");
+      expect(testForm).toHaveTextContent(
+        "This is the tile version of the Radio component."
+      );
+    });
+  });
+
+  describe("Snapshot tests", () => {
+    it("renders default Radio as expected", () => {
+      const { container } = render(
         <Radio
-          id="radio-tile"
-          isTile={true}
+          id="default-radio"
+          label="Default Radio"
+          name="default-group"
+          value="default-radio"
+        />
+      );
+      expect(container).toMatchSnapshot();
+    });
+
+    it("renders disabled Radio as expected", () => {
+      const { container } = render(
+        <Radio
+          disabled
+          id="default-radio"
+          label="Default Radio"
+          name="default-group"
+          value="default-radio"
+        />
+      );
+      expect(container).toMatchSnapshot();
+    });
+
+    it("renders tile Radio as expected", () => {
+      const { container } = render(
+        <Radio
+          id="tile-radio"
+          isTile
           label="Tile Radio"
           name="tile-group"
-          tileDescription="This is the tile version of the Radio component."
+          tileDescription="This is the tile Radio description."
+          value="tile-radio"
         />
-      </form>
-    );
-  });
+      );
+      expect(container).toMatchSnapshot();
+    });
 
-  it("renders as a tile", () => {
-    const r = screen.getByRole("radio");
-    expect(r).toHaveClass("usa-radio__input--tile");
-  });
-
-  it("is unchecked by default", () => {
-    const r = screen.getByRole("radio");
-    expect(r).not.toBeChecked();
-  });
-
-  it("becomes checked on click", () => {
-    const r = screen.getByRole("radio");
-    fireEvent.click(r);
-    expect(r).toBeChecked();
-  });
-
-  it("renders and has the expected label", () => {
-    const r = screen.getByRole("radio");
-    expect(r).toBeInTheDocument();
-  });
-
-  it("has the expected ID", () => {
-    const r = screen.getByRole("radio");
-    expect(r.getAttribute("id")).toBe("radio-tile");
-  });
-
-  it("has the expected name attribute", () => {
-    const r = screen.getByRole("radio");
-    expect(r.getAttribute("name")).toBe("tile-group");
-  });
-
-  it("has the expected description text", () => {
-    const testForm = screen.getByTestId("test-form");
-    expect(testForm).toHaveTextContent(
-      "This is the tile version of the Radio component."
-    );
+    it("renders disabled tile Radio as expected", () => {
+      const { container } = render(
+        <Radio
+          disabled
+          id="tile-radio"
+          isTile
+          label="Tile Radio"
+          name="tile-group"
+          tileDescription="This is the tile Radio description."
+          value="tile-radio"
+        />
+      );
+      expect(container).toMatchSnapshot();
+    });
   });
 });
