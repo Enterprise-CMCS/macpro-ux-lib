02ca1941740d196917ce2cabaac541073518b771
jphechter <joseph.hechter@gmail.com>
2022-08-11 18:47:45 +0000

    Deploying to gha-pages from  @ 8d28d90324b26ec446df94d4066d359858fec247 🚀

---
+++ components/Header/ActionsMenu.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Icon, IconChoice } from "components/Icon/Icon";
+import { Link, LinkProps } from "components/Link/Link";
+
+interface ActionsMenuLink extends LinkProps {
+  iconName?: IconChoice;
+}
+
+interface Props {
+  name: string;
+  links: ActionsMenuLink[];
+}
+
+/**
+ * ActionsMenu Component
+ *
+ * @param {string} name   The name of the menu.
+ * @param {ActionsMenuLink[]} links  List of links to display in the menu.
+ */
+
+export const ActionsMenu: React.FC<Props> = ({ name, links, ...rest }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="user-actions-container">
+      <span className="menu-label" onClick={() => setIsOpen(!isOpen)}>
+        {name}
+        <Icon
+          aria-hidden="true"
+          name={isOpen ? "arrow_drop_up" : "arrow_drop_down"}
+          color="#fff"
+        />
+      </span>
+      <menu
+        className="user-actions-menu usa-nav__secondary-links"
+        style={isOpen ? { display: "block" } : {}}
+        {...rest}
+      >
+        {links.map((linkData, idx) => {
+          const { iconName, ...link } = linkData; // filter iconName out of linkData
+          return (
+            <li key={idx}>
+              <Link {...link}>
+                {iconName && <Icon name={iconName} color="#fff" />}
+                {link.text}
+              </Link>
+            </li>
+          );
+        })}
+      </menu>
+    </div>
+  );
+};
