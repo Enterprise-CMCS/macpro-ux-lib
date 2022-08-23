e5512a5d734a828c9c17e87b94ecda7081cdd977
JP Hechter <jhechter@fearless.tech>
2022-08-23 14:56:20 -0400

    additional functionality for AccordionGroup; better docs; data driven story

--- src/components/Accoridon/AccordionGroup.tsx
+++ src/components/Accoridon/AccordionGroup.tsx
@@ -3,7 +3,9 @@ import React, { PropsWithChildren } from "react";
 type IntrinsicElements = JSX.IntrinsicElements["div"];
 
 interface Props extends IntrinsicElements {
+  bordered?: boolean;
   className?: string;
+  multiSelect?: boolean;
 }
 
 /**
@@ -19,20 +21,27 @@ interface Props extends IntrinsicElements {
  * </AccordionGroup>
  * ```
  *
+ * @param {boolean}          bordered     Determines whether or not a border is shown around each Accordion item in the group.
  * @param {string}           className    Additional classes that will be applied to the AccordionGroup root div.
  * @param {React.ReactNode}  children     React components that will be rendered within the AccordionGroup
  * @param {string}           id           Unique identifier that will be applied to the AccordionGroup root div.
+ * @param {boolean}          multiSelect  Determines whether or not multiple Accordion items can be expanded at the same time.
  */
 export const AccordionGroup: React.FC<PropsWithChildren<Props>> = ({
+  bordered = false,
   className,
   children,
   id,
+  multiSelect,
   ...rest
 }) => {
   return (
     <div
-      className={`usa-accordion${className ? ` ${className}` : ""}`}
+      className={`usa-accordion${bordered ? " usa-accordion--bordered" : ""}${
+        multiSelect ? " usa-accordion--multiselectable" : ""
+      }${className ? ` ${className}` : ""}`}
       id={id}
+      data-allow-multiple={multiSelect}
       {...rest}
     >
       {children}
