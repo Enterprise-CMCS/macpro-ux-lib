428e65a7a0e4b26cce00b629b249ef73eb739c94
Gavin St. Ours <gst-ours@fearless.tech>
2022-08-25 16:59:14 -0400

    Replace USWDS JS with component code

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,35 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../Button/Button";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { useEffect, useState } from "@storybook/addons";
 import { Modal } from "./Modal";
 
 export default {
   title: "Components/Modal",
   component: Modal,
   argTypes: {
-    type: {
+    modalType: {
       description:
         "The type of Modal. Options include `default`, `large`, or `forcedAction`.",
     },
   },
+  args: {
+    description: "You have unsaved changes that will be lost.",
+    heading: "Are you sure you want to continue?",
+    modalType: "default",
+    primaryButtonText: "Primary Button",
+    secondaryButtonText: "Secondary Button",
+  },
 } as ComponentMeta<typeof Modal>;
 
 const Template: ComponentStory<typeof Modal> = ({ ...args }) => {
+  const [isOpen, setOpen] = useState(false);
+  const toggleModal = () => {
+    setOpen(!isOpen);
+  };
+
   return (
     <>
-      <a
-        className="usa-button"
-        aria-controls="example-modal-1"
-        data-open-modal
-      >
-        Open default modal
-      </a>
-
-      <Modal {...args} />
+      <Button buttonText="Open deftaul modal" onClick={toggleModal} />
+      <Modal
+        {...args}
+        isOpen={isOpen}
+        onClose={toggleModal}
+        primaryOnClick={toggleModal}
+        secondaryOnClick={toggleModal}
+      />
     </>
   );
 };
 
 export const Default = Template.bind({});
-Default.args = { type: "default" };
+Default.args = {};
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,82 +1,95 @@
-import React, { useState } from "react";
+import { Button } from "../Button/Button";
+import { Icon } from "../Icon/Icon";
+import React from "react";
+
+type ModalType = "default" | "large" | "forcedAction";
 
 interface Props {
-  modalType: "default" | "large" | "forcedAction";
+  description: string;
+  heading: string;
+  isOpen?: boolean;
+  modalType?: ModalType;
+  onClose: () => void;
+  primaryButtonText: string;
+  primaryOnClick: () => void;
+  secondaryButtonText: string;
+  secondaryOnClick: () => void;
 }
 
-export const Modal: React.FC<Props> = ({ modalType }) => {
+export const Modal: React.FC<Props> = ({
+  description,
+  heading,
+  isOpen = false,
+  modalType = "default",
+  onClose,
+  primaryButtonText,
+  primaryOnClick,
+  secondaryButtonText,
+  secondaryOnClick,
+}) => {
   return (
     <>
-      <div
-        className="usa-modal-wrapper"
-        role="dialog"
-        id="example-modal-1"
-        aria-labelledby="modal-1-heading"
-        aria-describedby="modal-1-description"
-      >
+      {isOpen && (
         <div
-          className="usa-modal-overlay"
-          aria-controls="example-modal-1"
-          data-ol-has-click-handler
+          className="usa-modal-wrapper"
+          role="dialog"
+          id="example-modal-1"
+          aria-labelledby="modal-1-heading"
+          aria-describedby="modal-1-description"
         >
-          <div
-            className="usa-modal"
-            id="example-modal-1"
-            aria-labelledby="modal-1-heading"
-            aria-describedby="modal-1-description"
-          >
-            <div className="usa-modal__content">
-              <div className="usa-modal__main">
-                <h2 className="usa-modal__heading" id="modal-1-heading">
-                  Are you sure you want to continue?
-                </h2>
-                <div className="usa-prose">
-                  <p id="modal-1-description">
-                    You have unsaved changes that will be lost.
-                  </p>
+          <div className="usa-modal-overlay" aria-controls="example-modal-1">
+            <div
+              className={`usa-modal ${
+                modalType === "large" ? "usa-modal--lg" : ""
+              }`}
+              id="example-modal-1"
+              aria-labelledby="modal-1-heading"
+              aria-describedby="modal-1-description"
+            >
+              <div className="usa-modal__content">
+                <div className="usa-modal__main">
+                  <h2 className="usa-modal__heading" id="modal-1-heading">
+                    {heading}
+                  </h2>
+                  <div className="usa-prose">
+                    <p id="modal-1-description">{description}</p>
+                  </div>
+                  <div className="usa-modal__footer">
+                    <ul className="usa-button-group">
+                      <li className="usa-button-group__item">
+                        <Button
+                          buttonText={primaryButtonText}
+                          onClick={primaryOnClick}
+                        />
+                      </li>
+                      <li>
+                        <Button
+                          buttonText={secondaryButtonText}
+                          buttonVariation="link"
+                          type="button"
+                          className="usa-button--unstyled padding-105"
+                          onClick={secondaryOnClick}
+                        />
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-                <div className="usa-modal__footer">
-                  <ul className="usa-button-group">
-                    <li className="usa-button-group__item">
-                      <button
-                        type="button"
-                        className="usa-button"
-                        data-close-modal
-                      >
-                        Continue without saving
-                      </button>
-                    </li>
-                    <li>
-                      <button
-                        type="button"
-                        className="usa-button usa-button--unstyled padding-105 text-center"
-                        data-close-modal
-                      >
-                        Go Back
-                      </button>
-                    </li>
-                  </ul>
-                </div>
+                {modalType !== "forcedAction" && (
+                  <button
+                    type="button"
+                    className="usa-button usa-modal__close"
+                    aria-label="Close this window"
+                    data-close-modal
+                    onClick={onClose}
+                  >
+                    <Icon name="close" />
+                  </button>
+                )}
               </div>
-              <button
-                type="button"
-                className="usa-button usa-modal__close"
-                aria-label="Close this window"
-                data-close-modal
-              >
-                <svg
-                  className="usa-icon"
-                  aria-hidden="true"
-                  focusable="false"
-                  role="img"
-                >
-                  {/* <use xlink:href="/assets/img/sprite.svg#close"></use> */}
-                </svg>
-              </button>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
