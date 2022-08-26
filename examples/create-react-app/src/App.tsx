57a415941e26d11fc134ca7412bb909f2d99a1ee
James Dinh <jdinh@collabralink.com>
2022-08-26 12:24:58 -0700

    add back examples

---
+++ examples/create-react-app/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@enterprise-cmcs/macpro-ux-lib";
+
+export const App = () => {
+  return (
+    <>
+      <Button>Demo Button</Button>
+    </>
+  );
+};
---
+++ examples/create-react-app/src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "@enterprise-cmcs/macpro-ux-lib/build/assets/css/index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
