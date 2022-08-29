276c3b643615221d9612506c7883a5b6f455cb8a
James Dinh <jdinh@collabralink.com>
2022-08-29 10:40:15 -0700

    fix import

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import "../src/assets/css/styles.css";
 import "../src/assets/theme/styles.scss";
-import "../sr-calendar/dist/Calendar.css";
+import "react-calendar/dist/Calendar.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
