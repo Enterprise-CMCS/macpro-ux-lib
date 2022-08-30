8e575a330d73d391de4f270a9bce6b684627f69e
Gavin St. Ours <gst-ours@fearless.tech>
2022-08-30 09:39:13 -0400

    Export Modal from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ import { Header } from "./components/Header/Header";
 import { Icon } from "./components/Icon/Icon";
 import { Link } from "./components/Link/Link";
 import { Logo } from "./components/Logo/Logo";
+import { Modal } from "./components/Modal/Modal";
 import { Radio } from "./components/Radio/Radio";
 import { RadioGroup } from "./components/RadioGroup/RadioGroup";
 import { TextArea } from "./components/TextArea/TextArea";
@@ -31,6 +32,7 @@ export {
   Icon,
   Link,
   Logo,
+  Modal,
   Radio,
   RadioGroup,
   TextArea,
