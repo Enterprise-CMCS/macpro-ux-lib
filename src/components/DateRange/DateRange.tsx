e786e3bcb689d34631d3850e51891efed79f6c33
James Dinh <jdinh@collabralink.com>
2022-09-21 07:24:00 -0700

    cleanup

--- src/components/DateRange/DateRange.tsx
+++ src/components/DateRange/DateRange.tsx
@@ -21,14 +21,14 @@ export interface Props extends IntrinsicElements {
  * DateRange Component
  * @param {string}  [defaultStartDate]     The start date picker input will set this value by default if it is a valid date. The date should be in the format mm/dd/yyyy.
  * @param {string}  [startDate]            The value given to the start date input. The date should be in the format mm/dd/yyyy.
- * @param {string}  [startInputId]         A unique identifier for the start date input.
- * @param {string}  [startInputName]       Name of the start date input field.
- * @param {string}  [startLabel]           The label of the start date input.
+ * @param {string}  startInputId           A unique identifier for the start date input.
+ * @param {string}  startInputName         Name of the start date input field.
+ * @param {string}  startLabel             The label of the start date input.
  * @param {string}  [defaultEndDate]       The end date picker input will set this value by default if it is a valid date. The date should be in the format mm/dd/yyyy.
  * @param {string}  [endDate]              The value given to the end date input. The date should be in the format mm/dd/yyyy.
- * @param {string}  [endInputId]           A unique identifier for the end date input.
- * @param {string}  [endInputName]         Name of the end date input field.
- * @param {string}  [endLabel]             The label of the end date input.
+ * @param {string}  endInputId             A unique identifier for the end date input.
+ * @param {string}  endInputName           Name of the end date input field.
+ * @param {string}  endLabel               The label of the end date input.
  * @param {string}  [disabled]             Controls whether or not the date range pickers are disabled to the user.
  * @param {boolean} [hint]                 Boolean that shows or hide the date format hint for both inputs, in the format mm/dd/yyyy.
  */
