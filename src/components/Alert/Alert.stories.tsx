a0cf0097012b88d82f39508346fb7b9f3ef0f078
James Dinh <jdinh@collabralink.com>
2022-09-21 08:51:03 -0700

    alert logic

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -5,8 +5,36 @@ import { Alert } from "./Alert";
 export default {
   title: "COMPONENTS/Alert",
   component: Alert,
-  args: {},
-  argTypes: {},
+  args: {
+    alertHeading: "Heading",
+    alertBody: "Body",
+  },
+  argTypes: {
+    alertHeading: {
+      description: "Text content for the alert heading.",
+    },
+    alertBody: {
+      description: "Text content for the alert body.",
+    },
+    variation: {
+      description: "Determines the alert variation to render.",
+    },
+    slim: {
+      description: "Renders a slimmer version of the banner.",
+    },
+    icon: {
+      description:
+        "Show/hide the alert icon associated with the variation of the alert.",
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "An alert keeps users informed of important and sometimes time-sensitive changes.",
+      },
+    },
+  },
 } as ComponentMeta<typeof Alert>;
 
 const Template: ComponentStory<typeof Alert> = ({ ...rest }) => (
@@ -14,4 +42,33 @@ const Template: ComponentStory<typeof Alert> = ({ ...rest }) => (
 );
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  variation: "info",
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  variation: "warning",
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  variation: "error",
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  variation: "success",
+};
+
+export const Slim = Template.bind({});
+Slim.args = {
+  slim: true,
+  variation: "success",
+};
+
+export const NoIcon = Template.bind({});
+NoIcon.args = {
+  icon: false,
+  variation: "success",
+};
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,14 +1,43 @@
 import React from "react";
 
-type IntrinsicElements = JSX.IntrinsicElements["p"];
+type IntrinsicElements = JSX.IntrinsicElements["div"];
 
-interface Props extends IntrinsicElements {}
+interface Props extends IntrinsicElements {
+  alertHeading: string;
+  alertBody: string;
+  variation: "info" | "warning" | "error" | "success";
+  slim?: boolean;
+  icon?: boolean;
+}
 
 /**
  * **Alert Component**
  *
- * @param {string}    id  Unique identifier required for each Accordion item used for form control.
+ * @param {string}     alertHeading   Text content for the alert heading.
+ * @param {string}     alertBody      Text content for the alert body.
+ * @param {string}     variation      Determines the alert variation to render.
+ * @param {boolean}    slim           Renders a slimmer version of the banner.
+ * @param {boolean}    icon           Show/hide the alert icon associated with the variation of the alert.
  */
-export const Alert: React.FC<Props> = ({ ...rest }) => {
-  return <p>Test</p>;
+export const Alert: React.FC<Props> = ({
+  alertHeading,
+  alertBody,
+  variation,
+  slim = false,
+  icon = true,
+  ...rest
+}) => {
+  return (
+    <div
+      className={`usa-alert usa-alert--${variation}${
+        slim ? " usa-alert--slim" : ""
+      }${icon ? "" : " usa-alert--no-icon"}`}
+      {...rest}
+    >
+      <div className="usa-alert__body">
+        <h4 className="usa-alert__heading">{alertHeading}</h4>
+        <p className="usa-alert__text">{alertBody}</p>
+      </div>
+    </div>
+  );
 };
