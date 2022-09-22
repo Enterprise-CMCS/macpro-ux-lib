f06dbe42ffe06b95943d34a815ce085256888a36
James Dinh <jdinh@collabralink.com>
2022-09-22 13:52:11 -0700

    add date range into example

--- examples/macpro-example/src/App.tsx
+++ examples/macpro-example/src/App.tsx
@@ -215,12 +215,22 @@ function App() {
           id="child-textarea"
           label="Child 1 TextArea"
         />
+        <UX.Datefield id="1" name="react-calendar" label="Calendar example" />
         <UX.TextInput
           fieldName="text-input"
           id="text-input-1"
           label="The Text Input We've All Been Waiting For"
         ></UX.TextInput>
-        <UX.Datefield id="1" name="react-calendar" label="Calendar example" />
+        <UX.DateRange
+          startInputId="id-1"
+          startInputName="react-calendar"
+          startLabel="Start Range"
+          endInputId="id-2"
+          endInputName="react-calendar2"
+          endLabel="End Range"
+          defaultStartDate="08/08/2022"
+          hint={true}
+        />
       </div>
       <UX.Footer emailAddress="jhechter@fearless.tech" />
     </div>
