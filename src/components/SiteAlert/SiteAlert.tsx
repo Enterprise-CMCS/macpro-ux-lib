d1a40dcb671e37d5c8bbc5bdc0569a20a33dbc63
James Dinh <jdinh@collabralink.com>
2022-09-23 08:32:10 -0700

    updating logic

--- src/components/SiteAlert/SiteAlert.tsx
+++ src/components/SiteAlert/SiteAlert.tsx
@@ -4,9 +4,9 @@ import { Icon } from "../Icon/Icon";
 type IntrinsicElements = JSX.IntrinsicElements["div"];
 
 interface Props extends IntrinsicElements {
-  alertHeading: string;
+  alertHeading?: string;
   alertBody: string;
-  variation: "info" | "warning" | "error" | "success";
+  emergency: boolean;
   slim?: boolean;
   icon?: boolean;
   close?: React.MouseEventHandler<HTMLButtonElement>;
@@ -17,7 +17,7 @@ interface Props extends IntrinsicElements {
  *
  * @param {string}                     alertHeading   Text content for the alert heading.
  * @param {string}                     alertBody      Text content for the alert body.
- * @param {string}                     variation      Determines the alert variation to render.
+ * @param {string}                     emergency      Determines the alert whether to render the information or emergency alert.
  * @param {boolean}                    slim           Renders a slimmer version of the banner.
  * @param {boolean}                    icon           Show/hide the alert icon associated with the variation of the alert.
  * @param {React.MouseEventHandler}    close          Close button that shows when a event handler is passed to the component.
@@ -25,7 +25,7 @@ interface Props extends IntrinsicElements {
 export const SiteAlert: React.FC<Props> = ({
   alertHeading,
   alertBody,
-  variation,
+  emergency = false,
   slim = false,
   icon = true,
   close,
@@ -36,12 +36,12 @@ export const SiteAlert: React.FC<Props> = ({
     <section
       className="usa-site-alert usa-site-alert--info"
       aria-label="Site alert,"
+      {...rest}
     >
       <div className="usa-alert">
         <div className="usa-alert__body">
-          <h3 className="usa-alert__heading">Short alert message</h3>
+          <h3 className="usa-alert__heading">{alertHeading}</h3>
           <p className="usa-alert__text">
-            Additional context and followup information including
             <a className="usa-link" href="javascript:void(0);">
               a link
             </a>
