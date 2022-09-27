02e81591a73e9cf77b4df0da8f8408a531dfa1b6
Gavin St. Ours <gst-ours@fearless.tech>
2022-09-27 09:30:36 -0400

    Finish tests for both variations

--- src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -19,6 +19,30 @@ describe("Breadcrumbs", () => {
       const b = screen.getByTestId("test-breadcrumbs");
 
       expect(b).toBeInTheDocument;
+      expect(b).toHaveTextContent("First item");
+      expect(b).toHaveTextContent("Second item");
+      expect(b).toHaveTextContent("Current Item");
+    });
+  });
+
+  describe("parent only", () => {
+    it("renders parentOnly version of Breadcrumbs as expected", () => {
+      render(
+        <div data-testid="test-breadcrumbs">
+          <Breadcrumbs
+            currentItemName="Current Item"
+            items={items}
+            parentOnly={true}
+          />
+        </div>
+      );
+
+      const b = screen.getByTestId("test-breadcrumbs");
+
+      expect(b).toBeInTheDocument;
+      expect(b).toHaveTextContent("First item");
+      expect(b).not.toHaveTextContent("Second item");
+      expect(b).not.toHaveTextContent("Current Item");
     });
   });
 });
