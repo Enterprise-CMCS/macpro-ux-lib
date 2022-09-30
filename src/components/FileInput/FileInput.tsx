d06c8be54ce4129d457fb17a5b91ca9d7342cbda
James Dinh <jdinh@collabralink.com>
2022-09-30 13:15:09 -0700

    laying out component

--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-type IntrinsicElements = JSX.IntrinsicElements["p"];
+type IntrinsicElements = JSX.IntrinsicElements["input"];
 
 interface Props extends IntrinsicElements {}
 
@@ -10,5 +10,18 @@ interface Props extends IntrinsicElements {}
  * @param {string}    id  Unique identifier required for each Accordion item used for form control.
  */
 export const FileInput: React.FC<Props> = ({ ...rest }) => {
-  return <p>Test</p>;
+  return (
+    <div className="usa-form-group" {...rest}>
+      <label className="usa-label" htmlFor="file-input-single">
+        Input accepts a single file
+      </label>
+      <input
+        id="file-input-single"
+        className="usa-file-input"
+        type="file"
+        name="file-input-single"
+        {...rest}
+      />
+    </div>
+  );
 };
