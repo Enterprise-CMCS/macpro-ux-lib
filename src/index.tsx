49b6a59976a0eb3d8e2f96f1e7fbed8993be5f1e
Gavin St. Ours <gst-ours@fearless.tech>
2022-09-30 09:52:48 -0400

    [OY2-20680] Breadcrumbs (#126)
    * Set up Breadcrumbs component
    
    * Update parameters
    
    * Add both styles of breadcrumbs
    
    * Add custom SCSS
    
    * Add Bradcrumbs to Storybook
    
    * Add default value for parentOnly flag
    
    * Initial test
    
    * Ensure list items have keys
    
    * Finish tests for both variations
    
    * Add description
    
    * Add ParentOnly story
    
    * Fix link colors
    
    * Make currentItem optional
    
    * Remove unnecessary imports
    
    * Use inline-flex to prevent container from auto-expanding to parent width
    
    * Add onClick parameter

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { Accordion } from "./components/Accordion/Accordion";
 import { AccordionGroup } from "./components/Accordion/AccordionGroup";
 import { ActionsMenu } from "./components/Header/ActionsMenu";
 import { Alert } from "./components/Alert/Alert";
+import { Breadcrumbs } from "./components/Breadcrumbs/Breadcrumbs";
 import { Button } from "./components/Button/Button";
 import { Card } from "./components/Card/Card";
 import { Datefield } from "./components/Datefield/Datefield";
@@ -37,6 +38,7 @@ export {
   AccordionGroup,
   ActionsMenu,
   Alert,
+  Breadcrumbs,
   Button,
   Card,
   CardChoice,
