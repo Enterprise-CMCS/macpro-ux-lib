eb9b27bd3224dc879c2e1711a6aca172f57ecf7a
JP Hechter <jhechter@fearless.tech>
2022-10-04 12:56:41 -0400

    programatically generate tables based on data provided

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useDebugValue } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Table } from "./Table";
+import data from "./data.json";
 
 export default {
   title: "COMPONENTS/Table",
   component: Table,
-  args: {},
+  args: {
+    caption:
+      "Standard Table: A commonly used table with many features and functionality",
+    data: data["standardTable"],
+  },
   argTypes: {},
 } as ComponentMeta<typeof Table>;
 
@@ -13,5 +18,25 @@ const Template: ComponentStory<typeof Table> = ({ ...rest }) => (
   <Table {...rest} />
 );
 
-export const Default = Template.bind({});
-Default.args = {};
+export const StandardTable = Template.bind({});
+StandardTable.args = {};
+
+export const BorderlessTable = Template.bind({});
+BorderlessTable.args = {
+  borderless: true,
+  data: data["standardTable"],
+  caption:
+    "Borderless table: A borderless table can be useful when you want the information to feel more a part of the text it accompanies and extends.",
+};
+
+export const ScrollableTable = Template.bind({});
+ScrollableTable.args = {
+  data: data["scrollableTable"],
+  footnote: "* in billions of dollars. Data for illustration purposes only.",
+  scrollable: true,
+};
+
+export const SortableTable = Template.bind({});
+SortableTable.args = {
+  data: data["sortableTable"],
+};
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -2,150 +2,82 @@ import React from "react";
 
 type IntrinsicElements = JSX.IntrinsicElements["table"];
 
-interface Props extends IntrinsicElements {}
+interface Props extends IntrinsicElements {
+  borderless?: boolean;
+  caption?: string;
+  compact?: string;
+  footnote?: string;
+  data: any;
+  scrollable?: boolean;
+  stacked?: boolean;
+  stackedHeader?: boolean;
+  striped?: boolean;
+}
 
 /**
  * **Table Component**
  *
  * @param {string}    id  Unique identifier required for each Accordion item used for form control.
  */
-export const Table: React.FC<Props> = ({ ...rest }) => {
+export const Table: React.FC<Props> = ({
+  borderless,
+  caption,
+  compact,
+  footnote,
+  data,
+  id,
+  scrollable,
+  stacked,
+  stackedHeader,
+  striped,
+  ...rest
+}) => {
+  // TODO: is there a way to make this more flexible to
+  // support vertical headings and other orientations?
+
+  const tableClassNames = `usa-table${
+    borderless ? " usa-table--borderless" : ""
+  }${compact ? " usa-table--compact" : ""}${
+    stacked ? ` usa-table--stacked${stackedHeader ? "-header" : ""}` : ""
+  }${striped ? " usa-table--striped" : ""}`;
+
+  const buildRow = (row: any, header?: boolean) => {
+    return (
+      <tr>
+        {row.map((cell: any, idx: number) => {
+          return header ? (
+            <th scope="col" key={`${id}-column-header-${idx}`} {...cell.props}>
+              {cell.value}
+            </th>
+          ) : (
+            <td
+              className="text-tabular"
+              key={`${id}-row-cell-${idx}`}
+              {...cell.props}
+            >
+              {cell.value}
+            </td>
+          );
+        })}
+      </tr>
+    );
+  };
+
   return (
-    <>
-      <table className="usa-table" {...rest}>
-        <caption>Scrollable table</caption>
-        <colgroup>
-          <col />
-        </colgroup>
-        <colgroup span={2}></colgroup>
-        <colgroup span={2}></colgroup>
-        <thead>
-          <tr>
-            <th rowSpan={2}>
-              Federal Budget
-              <br />
-              Baseline Projections
-            </th>
-            <th colSpan={2} scope="colgroup" className="text-center">
-              2017
-            </th>
-            <th colSpan={2} scope="colgroup" className="text-center">
-              2018
-            </th>
-            <th colSpan={2} scope="colgroup" className="text-center">
-              2019
-            </th>
-            <th colSpan={2} scope="colgroup" className="text-center">
-              2020
-            </th>
-            <th colSpan={2} scope="colgroup" className="text-center">
-              2021
-            </th>
-            <th colSpan={2} scope="colgroup" className="text-center">
-              Hist. Avg.
-            </th>
-          </tr>
-          <tr>
-            <th scope="col" className="text-right">
-              %GDP
-            </th>
-            <th scope="col" className="text-right">
-              USD*
-            </th>
-            <th scope="col" className="text-right">
-              %GDP
-            </th>
-            <th scope="col" className="text-right">
-              USD*
-            </th>
-            <th scope="col" className="text-right">
-              %GDP
-            </th>
-            <th scope="col" className="text-right">
-              USD*
-            </th>
-            <th scope="col" className="text-right">
-              %GDP
-            </th>
-            <th scope="col" className="text-right">
-              USD*
-            </th>
-            <th scope="col" className="text-right">
-              %GDP
-            </th>
-            <th scope="col" className="text-right">
-              USD*
-            </th>
-            <th scope="col" className="text-right">
-              %GDP
-            </th>
-            <th scope="col" className="text-right">
-              USD*
-            </th>
-          </tr>
-        </thead>
-        <tr>
-          <th scope="row">Revenue</th>
-          <td className="font-mono-sm text-tabular text-right">17.2%</td>
-          <td className="font-mono-sm text-tabular text-right">3,316</td>
-          <td className="font-mono-sm text-tabular text-right">16.4%</td>
-          <td className="font-mono-sm text-tabular text-right">3,338</td>
-          <td className="font-mono-sm text-tabular text-right">16.3%</td>
-          <td className="font-mono-sm text-tabular text-right">3,490</td>
-          <td className="font-mono-sm text-tabular text-right">16.7%</td>
-          <td className="font-mono-sm text-tabular text-right">3,678</td>
-          <td className="font-mono-sm text-tabular text-right">16.7%</td>
-          <td className="font-mono-sm text-tabular text-right">3,827</td>
-          <td className="font-mono-sm text-tabular text-right">17.4%</td>
-          <td className="font-mono-sm text-tabular text-right">3,381</td>
-        </tr>
-        <tr>
-          <th scope="row">Outlays</th>
-          <td className="font-mono-sm text-tabular text-right">20.6%</td>
-          <td className="font-mono-sm text-tabular text-right">3,982</td>
-          <td className="font-mono-sm text-tabular text-right">20.2%</td>
-          <td className="font-mono-sm text-tabular text-right">4,142</td>
-          <td className="font-mono-sm text-tabular text-right">21.0%</td>
-          <td className="font-mono-sm text-tabular text-right">4,470</td>
-          <td className="font-mono-sm text-tabular text-right">21.3%</td>
-          <td className="font-mono-sm text-tabular text-right">4,685</td>
-          <td className="font-mono-sm text-tabular text-right">21.6%</td>
-          <td className="font-mono-sm text-tabular text-right">4,949</td>
-          <td className="font-mono-sm text-tabular text-right">20.3%</td>
-          <td className="font-mono-sm text-tabular text-right">4,198</td>
-        </tr>
-        <tr>
-          <th scope="row">Budget Deficit</th>
-          <td className="font-mono-sm text-tabular text-right">-3.5%</td>
-          <td className="font-mono-sm text-tabular text-right">-665</td>
-          <td className="font-mono-sm text-tabular text-right">-3.8%</td>
-          <td className="font-mono-sm text-tabular text-right">-804</td>
-          <td className="font-mono-sm text-tabular text-right">-4.6%</td>
-          <td className="font-mono-sm text-tabular text-right">-981</td>
-          <td className="font-mono-sm text-tabular text-right">-4.6%</td>
-          <td className="font-mono-sm text-tabular text-right">-1,008</td>
-          <td className="font-mono-sm text-tabular text-right">-4.9%</td>
-          <td className="font-mono-sm text-tabular text-right">-1,123</td>
-          <td className="font-mono-sm text-tabular text-right">-2.9%</td>
-          <td className="font-mono-sm text-tabular text-right">-483</td>
-        </tr>
-        <tr>
-          <th scope="row">Debt Held by Public</th>
-          <td className="font-mono-sm text-tabular text-right">76.0%</td>
-          <td className="font-mono-sm text-tabular text-right">14,665</td>
-          <td className="font-mono-sm text-tabular text-right">77.4%</td>
-          <td className="font-mono-sm text-tabular text-right">15,688</td>
-          <td className="font-mono-sm text-tabular text-right">79.2%</td>
-          <td className="font-mono-sm text-tabular text-right">16,762</td>
-          <td className="font-mono-sm text-tabular text-right">80.9%</td>
-          <td className="font-mono-sm text-tabular text-right">17,872</td>
-          <td className="font-mono-sm text-tabular text-right">83.1%</td>
-          <td className="font-mono-sm text-tabular text-right">18,998</td>
-          <td className="font-mono-sm text-tabular text-right">41.7%</td>
-          <td className="font-mono-sm text-tabular text-right">8,065</td>
-        </tr>
+    <div
+      className={`${scrollable ? "usa-table-container--scrollable" : ""}${
+        stacked ? " width-mobile" : ""
+      }`}
+      tabIndex={0}
+    >
+      <table className={tableClassNames} {...rest}>
+        {caption && <caption>{caption}</caption>}
+        {data.headers && (
+          <thead>{data.headers.map((row: any) => buildRow(row, true))}</thead>
+        )}
+        {data.body.map((row: any) => buildRow(row))}
       </table>
-      <p>* in billions of dollars. Data for illustration purposes only.</p>
-    </>
+      {footnote && <p className="usa-table__footnote">{footnote}</p>}
+    </div>
   );
 };
