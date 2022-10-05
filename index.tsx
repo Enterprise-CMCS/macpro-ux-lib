41205e94714434e59b435f281b652101eb3d5363
jdinh8124 <jdinh@collabralink.com>
2022-10-05 14:07:23 +0000

    Deploying to gha-pages from  @ fafa982dac6e16cbae13b0d494281185c562f88f 🚀

--- index.tsx
+++ index.tsx
@@ -25,6 +25,7 @@ import { Radio } from "./components/Radio/Radio";
 import { RadioGroup } from "./components/Radio/RadioGroup";
 import { SiteAlert } from "./components/SiteAlert/SiteAlert";
 import { Search } from "./components/Search/Search";
+import { StepIndicator } from "./components/StepIndicator/StepIndicator";
 import { TabPanel } from "./components/Tabs/TabPanel";
 import { Tabs } from "./components/Tabs/Tabs";
 import { TextArea } from "./components/TextArea/TextArea";
@@ -57,6 +58,7 @@ export {
   RadioGroup,
   Search,
   SiteAlert,
+  StepIndicator,
   TabPanel,
   Tabs,
   TextArea,
