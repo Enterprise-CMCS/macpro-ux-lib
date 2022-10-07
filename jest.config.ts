d5c20d447839be6c4e0aa9619bd2f090c8b6dd20
Gavin St. Ours <gst-ours@fearless.tech>
2022-10-07 15:28:33 -0400

    Remove moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,4 @@ export default {
     "^.+\\.(ts|tsx)$": "ts-jest", // That one tells Jest to use ts-jest when dealing with TypeScript files
     "^.+\\.(png|svg)$": "<rootDir>/imgTransform.js", // handle for images
   },
-  moduleNameMapper: {
-    "@uswds/(.*)": "<rootDir>/node_modules/@uswds/uswds",
-  },
 };
