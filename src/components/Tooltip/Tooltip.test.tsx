15782435b6b21bf5b945a37bca715b0e008b8ea0
Gavin St. Ours <gst-ours@fearless.tech>
2022-10-07 15:28:52 -0400

    Finish tests

--- src/components/Tooltip/Tooltip.test.tsx
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -1,44 +1,77 @@
 import { Tooltip } from "./Tooltip";
 import { render, screen } from "../../test-setup";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 
 describe("Tooltip", () => {
-  // beforeEach(() => {
-  //   render(
-  //     <Tooltip position="top" title="This is the tooltip">
-  //       <button className="test-button">Test</button>
-  //     </Tooltip>
-  //   );
-  // });
+  describe("Initialization", () => {
+    beforeEach(() => {
+      render(
+        <div data-testid="wrapper">
+          <Tooltip position="top" title="This is the tooltip">
+            <button className="test-button">Test</button>
+          </Tooltip>
+        </div>
+      );
+    });
 
-  it("renders the test button as expected", () => {
-    render(
-      <div data-testid="wrapper">
-        <Tooltip position="top" title="This is the tooltip">
-          <button className="test-button">Test</button>
-        </Tooltip>
-      </div>
-    );
+    it("renders the initial state as expected", () => {
+      const wrapper = screen.getByTestId("wrapper");
+      expect(wrapper).toBeInTheDocument();
+      expect(wrapper).toHaveTextContent("Test");
 
-    const wrapper = screen.getByTestId("wrapper");
-    expect(wrapper).toBeInTheDocument();
-    expect(wrapper).toHaveTextContent("Test");
+      const button = wrapper.getElementsByClassName("test-button")[0];
+      expect(button).toBeInTheDocument();
 
-    const button = wrapper.getElementsByClassName("test-button")[0];
-    expect(button).toBeInTheDocument();
-
-    // The tooltip text should be in the DOM
-    expect(wrapper).toHaveTextContent("This is the tooltip");
+      const tooltip = wrapper.getElementsByClassName("usa-tooltip__body")[0];
+      expect(tooltip).toBeInTheDocument();
+    });
   });
 
-  // it("renders the tooltip as expected", () => {
-  //   const container = render(
-  //     <Tooltip position="top" title="This is the tooltip">
-  //       <button className="test-button">Test</button>
-  //     </Tooltip>
-  //   ).container;
+  describe("Hover states", () => {
+    type PossiblePositions = "top" | "right" | "bottom" | "left";
+    const positions: Array<PossiblePositions> = [
+      "top",
+      "right",
+      "bottom",
+      "left",
+    ];
 
-  //   const tooltip = container.getElementsByClassName("usa-tooltip__body")[0];
-  //   expect(tooltip).toBeInTheDocument();
-  // });
+    for (const position of positions) {
+      const tooltipText = `The tooltip is on the ${position}`;
+
+      it(`renders the tooltip on the ${position}`, () => {
+        render(
+          <div data-testid="wrapper">
+            <Tooltip position={position} title={tooltipText}>
+              <button className="test-button">Test</button>
+            </Tooltip>
+          </div>
+        );
+
+        const wrapper = screen.getByTestId("wrapper");
+        const button = wrapper.getElementsByClassName("test-button")[0];
+        const tooltip = wrapper.getElementsByClassName("usa-tooltip__body")[0];
+
+        // Initial state
+        expect(tooltip).toBeVisible();
+        expect(tooltip).toHaveTextContent(tooltipText);
+        expect(tooltip).not.toHaveClass("is-set");
+        expect(tooltip).toHaveAttribute("aria-hidden", "true");
+        expect(tooltip).not.toHaveClass(`usa-tooltip__body--${position}`);
+
+        // Hover
+        userEvent.hover(button);
+        expect(tooltip).toHaveClass("is-set");
+        expect(tooltip).toHaveAttribute("aria-hidden", "false");
+        expect(tooltip).toHaveClass(`usa-tooltip__body--${position}`);
+
+        // Stop hovering
+        userEvent.unhover(button);
+        expect(tooltip).not.toHaveClass("is-set");
+        expect(tooltip).toHaveAttribute("aria-hidden", "true");
+        expect(tooltip).toHaveClass(`usa-tooltip__body--${position}`);
+      });
+    }
+  });
 });
