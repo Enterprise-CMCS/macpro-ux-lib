fc7ee74f2856586cff005ed57119cf4d35ba07e0
JP Hechter <jhechter@fearless.tech>
2022-10-12 10:59:13 -0400

    create oneMAC theme

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import oneMac_theme from "./themes/oneMAC_theme";
+
+addons.setConfig({
+  theme: oneMac_theme,
+});
---
+++ .storybook/themes/oneMAC_theme.js
@@ -0,0 +1,10 @@
+import { create } from "@storybook/theming";
+import logo from "./oneMAC_logo.svg";
+
+export default create({
+  base: "light",
+  brandTitle: "OneMAC UI Component Library",
+  brandUrl: "https://onemac.cms.gov/",
+  brandImage: logo,
+  brandTarget: "_blank",
+});
