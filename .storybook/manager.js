ceb842546dcce24787ca6f44721edbeb4080c449
JP Hechter <jhechter@fearless.tech>
2022-10-12 14:56:46 -0400

    use storybook-dark-mode and pass theme props to add-on

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from "@storybook/addons";
-import oneMac_theme from "./themes/oneMAC_theme";
-
-addons.setConfig({
-  theme: oneMac_theme,
-});
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,15 @@
 import "../src/assets/css/styles.css";
 import "../src/assets/theme/styles.scss";
 import "react-calendar/dist/Calendar.css";
+import { themes } from "@storybook/theming";
+import logo from "./oneMAC_logo.svg";
+
+const brandThemeParams = {
+  brandTitle: "OneMAC UI Component Library",
+  brandUrl: "https://onemac.cms.gov/",
+  brandImage: logo,
+  brandTarget: "_blank",
+};
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,5 +18,9 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  darkMode: {
+    dark: { ...themes.dark, ...brandThemeParams },
+    light: { ...themes.light, ...brandThemeParams },
   },
 };
